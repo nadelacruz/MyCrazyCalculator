3f5d99dd6553885f05f144ab6c6974fa5afd487a
nadelacruz <nicoangelodelacruz@gmail.com>
2021-04-12 09:42:26 +0800

    Separated InfixToPostfix method.

--- src/MyCrazyCalculator.java
+++ src/MyCrazyCalculator.java
@@ -23,7 +23,7 @@ public class MyCrazyCalculator {
     public double evaluate(String expression) {
         double finalOperand = -1;
         if (InfixChecker.checkInfix(expression)) {
-            finalOperand = Double.parseDouble(this.evaluatePostfix(this.InfixToPostfix(expression)));
+            finalOperand = Double.parseDouble(this.evaluatePostfix(InfixToPostfixConverter.ConvertInfixToPostfix(expression, this.stack, this.length)));
         } else {
             System.out.println("Error. Invalid infix expression detected. See MyCrazyCalculatorTester for the list of valid and invalid infix expressions.");
             System.exit(0);
@@ -89,121 +89,6 @@ public class MyCrazyCalculator {
 
         return toReturn;
     }
-
-    /** Converts a infix equation to a postfix equation.
-     * @param exp the expression to convert.
-     * @return the converted equation. */
-    public String InfixToPostfix(String exp) {
-        this.stack = new CalcStack(this.length);
-
-        StringBuilder result = new StringBuilder();
-        int foundIndex = 0;
-        int counter = 0;
-
-        while (foundIndex != -1) {
-            foundIndex = exp.indexOf(' ', foundIndex + 1);
-            counter++;
-        }
-
-        int indexer1 = 0;
-        int indexer2 = exp.indexOf(' ', indexer1);
-
-        for (int i = 0; i < counter; i++) {
-            String tempExp = exp.substring(indexer1, indexer2);
-
-            if (HelperMethods.isOperand(tempExp)) {
-                result.append(tempExp).append(" ");
-            } else if (HelperMethods.isOperator(tempExp)) {
-                String tempStr = "";
-                try {
-                    if (!(this.stack.isEmpty())) {
-                        tempStr = this.stack.peek();
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-                while(!(this.stack.isEmpty()) && !(tempStr.equals("(")) && this.HasHigherPrecedence(tempStr, tempExp)) {
-                    try {
-                        result.append(this.stack.pop()).append(" ");
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    try {
-                        if (!(this.stack.isEmpty())) {
-                            tempStr = this.stack.peek();
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                try {
-                    this.stack.push(tempExp);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            } else if (tempExp.equals("(")) {
-                try {
-                    this.stack.push(tempExp);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            } else if (tempExp.equals(")")) {
-                String tempStr = "";
-                try {
-                    if (!(this.stack.isEmpty())) {
-                        tempStr = this.stack.peek();
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-                while(!(this.stack.isEmpty()) && !(tempStr.equals("("))) {
-                    try {
-                        result.append(this.stack.pop()).append(" ");
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    try {
-                        if (!(this.stack.isEmpty())) {
-                            tempStr = this.stack.peek();
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                try {
-                    this.stack.pop();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-            indexer1 = indexer2 + 1;
-
-            if (i != counter - 2) {
-                indexer2 = exp.indexOf(' ', indexer1);
-            } else {
-                indexer2 = exp.length();
-            }
-        }
-        while (!(this.stack.isEmpty())) {
-            String tempStr = "";
-            try {
-                tempStr = this.stack.peek();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            result.append(tempStr).append(" ");
-            try {
-                this.stack.pop();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-
-        result.deleteCharAt(result.length() - 1);
-
-        return result.toString();
-    }
     
     /** Performs the calculation of the string operator and the operands.
      * @param operator the operator.
@@ -236,42 +121,5 @@ public class MyCrazyCalculator {
         }
 
         return x.toString();
-    }
-
-    /** Checks if exp1 has higher operator precedence than exp2.
-     * @param exp1 first operator.
-     * @param exp2 second operator.
-     * @return true if exp1 has higher precedence than exp 2, false if not. */
-    public boolean HasHigherPrecedence(String exp1, String exp2) {
-        int x = 0;
-        int y = 0;
-        switch (exp1) {
-            case "^":
-                x = 3;
-                break;
-            case "*":
-            case "/":
-                x = 2;
-                break;
-            case "+":
-            case "-":
-                x = 1;
-                break;
-        }
-
-        switch (exp2) {
-            case "^":
-                y = 3;
-                break;
-            case "*":
-            case "/":
-                y = 2;
-                break;
-            case "+":
-            case "-":
-                y = 1;
-                break;
-        }
-        return x >= y;
     }
 }
